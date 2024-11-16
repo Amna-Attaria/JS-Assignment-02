d4f6df091e78a37fc5bf1ca5a61d723bb9a2ae16
Amna-Attaria <khan12amna26@gmail.com>
2024-11-16 20:01:14 +0500

    JS Assignment Chapter: 9 to 11

---
+++ app.js
@@ -0,0 +1,164 @@
+// document.write("<h1>Qno:01</h1>");
+// var city = prompt("Write the name of city of light");
+// if ( city == "Karachi" )
+// {
+//     document.write("Wellcome the city of light" );
+// }
+
+// document.write("<h1>Qno:02</h1>");
+// var gender = prompt("Enter your Gender");
+// if (gender == "Male")
+// {
+//     document.write("Good Morning Sir" );
+// }
+// else if (gender == "Female")
+// {
+//     document.write("Good Morning Ma’am" );
+// }
+// else
+//  {
+//     document.write("incorrect" );
+// }
+
+// document.write("<h1>Qno:03</h1>");
+// var trafficlight = prompt(" Enter color of road traffic signal")
+// if (trafficlight === "Red")
+// {
+//     document.write("Must stoped");
+// }
+// else if (trafficlight=="Yellow")
+// {
+//     document.write("Ready to move");
+// }
+// else if (trafficlight=="Green")
+// {
+//     document.write("Move Now");
+// }
+
+
+// document.write("<h1>Qno:04</h1>");
+// var fuel = +prompt("Enter remaining fuel in car (in litres)");
+// if (fuel <= 0.25)
+// {
+//   document.write("Please refill the fuel in your car");
+// }
+
+// document.write("<h1>Qno:05</h1>");
+// firstConditions
+//  var a = 4;
+// if (++a === 5){
+// alert("given condition for variable a is true");
+// }
+// // SecondConditions
+// //  this condition is false 
+// var b = 82;
+// if (b++ === 83){
+// alert("given condition for variable b is true");
+// }
+// thirdConditions
+// condition 2 or 4 is true 
+// var c = 12;
+// if (c++ === 13){
+// alert("condition 1 is true");
+// }
+// if (c === 13){
+// alert("condition 2 is true");
+// }
+// if (++c < 14){
+// alert("condition 3 is true");
+// }
+// if(c === 14){
+// alert("condition 4 is true");
+// }
+// forthConditions
+// var materialCost = 20000;
+// var laborCost = 2000;
+// var totalCost = materialCost + laborCost;
+// if (totalCost === laborCost + materialCost){
+// alert("The cost equals");
+// }
+// fifthConditions
+// if (true){
+//     alert("True");
+//     }
+//     if (false){
+//     alert("False");
+//     }
+// fifthConditions
+// if("car" < "cat"){
+//     alert("car is smaller than cat");
+//     }
+// document.write("<h1>Qno:06</h1>");
+// var totalmarks = +prompt("Enter you Totalmarks");
+// var obtainedmarks = prompt("Enter you Obtainedmarks");
+// var percentage = (obtainedmarks/totalmarks)*100; 
+// document.write("<b>TotalMarks: </b>" + totalmarks + "<br>" + "<b>MarksObtained: </b>" + obtainedmarks + "<br>")
+
+// if (percentage>=80 )
+// {
+//     document.write("<b>Percentage is :</b>" + percentage +"<br>" + "<b>Grade is :</b>" + "A-One" + "<br>" + "<b>Remarks is :</b>" + "Excellent" + "<br>");
+// }
+// else if (percentage>=70)
+// {
+//     document.write("<b>Percentage is :</b>" + percentage +"<br>" + "<b>Grade is :</b>" + "A" + "<br>" + "<b>Remarks is :</b>" + "Welldone");
+// }
+// else if (percentage>=60)
+// {
+//     document.write("<b>Percentage is :</b>" + percentage +"<br>" + "<b>Grade is :</b>" + "B" + "<br>" + "<b>Remarks is :</b>" + "Good" + "<br>");
+// }
+// else if (percentage>=50)
+// {
+//     document.write("<b>Percentage is :</b>" + percentage +"<br>" + "<b>Grade is :</b>" + "C" + "<br>" + "<b>Remarks is :</b>" + "Better luck " + "<br>");
+// }
+// else 
+// {
+//     document.write("Failed!")
+// }
+// document.write("<h1>Qno:07</h1>");
+// document.write("<h1>LET PLAY GAME</h1>");
+// var num = prompt("ENTER A SECRETE NUM");
+// var secretNum = 6 ;
+// if (num == secretNum)
+// {
+//     document.write (" Your Win ");
+// }
+// else if (num == 7)
+// {
+//   document.write ("Close enough to the correct answer")
+// }
+// else ("You Loss");
+// document.write("<h1>Qno:08</h1>");
+// var num = prompt("Enter number is divisible by 3");
+// if (num % 3 ==0)
+// {
+//     document.write("the number is divisible by 3.")
+// }
+// document.write("<h1>Qno:09</h1>");
+// var num = prompt("Enter number ");
+// if (num % 2 ==0)
+// {
+//     document.write("this is even number.")
+// }
+// else{
+    
+//     document.write("this is Odd number.")
+// }
+
+// document.write("<h1>Qno:10</h1>");
+// var temp = +prompt("Enter the Tempratur");
+// if (temp >=40)
+// {
+//     document.write ("It is too hot outside")
+// }
+// else if (temp >=30)
+// {
+//     document.write ("The Weather today is Normal")
+// }
+// else if (temp >=20)
+// {
+//     document.write ("Today’s Weather is cool.")
+// }
+// else if (temp >=10)
+// {
+//     document.write ("OMG! Today’s weather is so Cool.")
+// }
